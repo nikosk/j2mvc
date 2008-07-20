61edaa549ed118c0b00f8c4d875224dfe0c080d7
nikos.nikos.nikos.nikos <nikos.nikos.nikos.nikos@f264b053-494f-0410-bddc-8b74c26815f4>
2008-07-20 09:22:31 +0000

    Added license header

--- j2mvc/src/java/gr/dsigned/jmvc/framework/Library.java
+++ j2mvc/src/java/gr/dsigned/jmvc/framework/Library.java
@@ -1,9 +1,22 @@
+/*
+ *  Library.java
+ * 
+ *  Copyright (C) 2008 Nikos Kastamoulas <nikosk@dsigned.gr>
+ * 
+ *  This module is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version. See http://www.gnu.org/licenses/lgpl.html.
+ * 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+package gr.dsigned.jmvc.framework;
 /**
  *
  * @author Nikosk <nikosk@dsigned.gr>
  */
-package gr.dsigned.jmvc.framework;
-
 public class Library {
     
 }
--- j2mvc/src/java/gr/dsigned/jmvc/models/Category.java
+++ j2mvc/src/java/gr/dsigned/jmvc/models/Category.java
@@ -1,7 +1,16 @@
-/**
- * 15 Μαρ 2008, gr.dsigned.jmvc.models 
- * Category.java
- * @author Nikosk <nikosk@dsigned.gr>
+/*
+ *  Category.java
+ * 
+ *  Copyright (C) 2008 Nikos Kastamoulas <nikosk@dsigned.gr>
+ * 
+ *  This module is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version. See http://www.gnu.org/licenses/lgpl.html.
+ * 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  */
 package gr.dsigned.jmvc.models;
 
@@ -12,7 +21,9 @@ import java.util.LinkedHashMap;
 import gr.dsigned.jmvc.db.Model;
 
 /**
- * 
+ * 15 Μαρ 2008, gr.dsigned.jmvc.models 
+ * Category.java
+ * @author Nikosk <nikosk@dsigned.gr>
  */
 public class Category extends Model {
 
