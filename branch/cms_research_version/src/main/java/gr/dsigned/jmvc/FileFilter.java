602ee79570a62798f92f92b406d7809bdb646a0e
nikos.nikos.nikos.nikos <nikos.nikos.nikos.nikos@f264b053-494f-0410-bddc-8b74c26815f4>
2008-07-29 11:17:31 +0000

    Experimental version.

---
+++ branch/cms_research_version/src/main/java/gr/dsigned/jmvc/FileFilter.java
@@ -0,0 +1,139 @@
+package gr.dsigned.jmvc;
+
+/**
+ *
+ * @author Chris Corbyn <chris@w3style.co.uk>
+ */
+
+/**
+ * Intercepts the request before it hits the front controller.
+ * This filter is checking if the request that's been passed is for an actual file.
+ * If the request is for a real file, the file is served and the servlet is skipped.
+ */
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.ServletException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import java.net.URL;
+import java.net.MalformedURLException;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Checks the request for real files and intercepts the controller where needed.
+ */
+public class FileFilter implements Filter {
+
+    /**
+     * Filter configuration.
+     */
+    protected FilterConfig filterConfig;
+
+    /**
+     * Required init method to get config.
+     * @param FilterConfig filterConfig
+     */
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Runs this filter.
+     * @param ServletRequest request
+     * @param ServletResponse response
+     * @param FilterChain filterChain
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String ctxPath = httpRequest.getContextPath();
+        String requestUri = httpRequest.getRequestURI().substring(ctxPath.length());
+        if (httpRequest.getMethod().equals("GET") && isFile(requestUri)) {
+            serveFile(requestUri, response);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    /**
+     * Checks if the servlet context is able to get a resource at this path.
+     * @param String path
+     * @return boolean
+     * @throws IOException
+     */
+    private boolean isFile(String path) throws IOException {
+        URL url = null;
+        ServletContext context = filterConfig.getServletContext();
+        try {
+            url = context.getResource(path);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+        if (url == null) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Serves the file if possible.
+     * If an error occurs, either a IOException will be raised, or nothing will be streamed
+     * depending upon the cause of the error.
+     * @param String path
+     * @param ServletResponse response
+     * @throws IOException
+     */
+    private void serveFile(String path, ServletResponse response) throws IOException {
+        URL url = null;
+        ServletContext context = filterConfig.getServletContext();
+        try {
+            url = context.getResource(path);
+        } catch (MalformedURLException e) {
+            return;
+        }
+        if (url == null) {
+            return;
+        }
+        String mimeType = context.getMimeType(path);
+        if (mimeType == null) {
+            mimeType = "application/octet-stream";
+        }
+        ServletOutputStream os = null;
+        InputStream is = null;
+        try {
+            os = response.getOutputStream();
+            is = url.openStream();
+            response.setContentType(mimeType);
+            response.setContentLength(is.available());
+            byte b[] = new byte[8192];
+            int c  = is.read(b);
+            while (c > -1) {
+                os.write(b);
+                os.flush();
+                c = is.read(b);
+            }
+        } finally {
+            if (os != null) {
+                os.close();
+            }
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    /**
+     * Required destroy() method.
+     */
+    public void destroy() {
+        filterConfig = null;
+    }
+}
+
---
+++ branch/cms_research_version/src/main/java/gr/dsigned/jmvc/controllers/Sites.java
@@ -0,0 +1,109 @@
+/*
+ *  Home.java
+ * 
+ *  Copyright (C) 2008 Nikos Kastamoulas <nikosk@dsigned.gr>
+ * 
+ *  This module is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version. See http://www.gnu.org/licenses/lgpl.html.
+ * 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ */
+package gr.dsigned.jmvc.controllers;
+
+import gr.dsigned.jmvc.db.Bean;
+import gr.dsigned.jmvc.framework.Controller;
+import gr.dsigned.jmvc.framework.Renderer;
+import gr.dsigned.jmvc.libraries.PageData;
+import gr.dsigned.jmvc.models.Issue;
+import gr.dsigned.jmvc.models.Site;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+/**
+ *
+ * @author Nikosk <nikosk@dsigned.gr>
+ */
+public class Sites extends Controller {
+
+    public Sites() throws Exception {
+    }
+
+    public void index() throws Exception {
+        PageData data = new PageData();
+        Site site = $.loadModel("Site"); // Load model
+        Issue issue = $.loadModel("Issue");
+        Renderer lr = $.loadRenderer("ListRenderer");
+        LinkedHashMap<String,ArrayList<Bean>> issues = new LinkedHashMap<String,ArrayList<Bean>>();
+        ArrayList<Bean> sites = site.getSites();  
+        for(Bean s : sites){
+            issues.put(s.get("label"), issue.getBySiteId(s.get("id")));
+        }
+        data.put("label", "");        
+        data.put("content", lr.runMethod("renderLists", sites, issues));
+        $.loadView("list", data);
+    }
+
+    public void add_site() throws Exception {
+        PageData data = new PageData();
+        Site site = $.loadModel("Site"); // Load model
+        ArrayList<Bean> sites = site.getSites();
+        if ($.input.post("label").isEmpty()) {
+            data.put("label", "");
+            data.put("action", "/sites/add_site/");
+            data.put("redirect_to", "/" + $.input.segment(0));
+            $.loadView("list_form", data);
+        } else {
+            site.insertSite($.input.post("label"));
+            $.response.sendRedirect($.input.post("redirect_to"));
+        }
+    }
+
+    public void delete_site() throws Exception {
+        String id = $.input.segment(2);
+        Site site = $.loadModel("Site"); // Load model
+        site.db.delete("sites", id);
+        $.response.sendRedirect("/sites");
+    }
+
+    public void edit_site() throws Exception {
+        String id = $.input.segment(2);
+        PageData data = new PageData();
+        Site site = $.loadModel("Site"); // Load model        
+        Renderer lr = $.loadRenderer("ListRenderer");
+        ArrayList<Bean> sites = site.getSites();
+        if ($.input.post("label").isEmpty()) {
+            site.load(id);
+            data.put("id", id);
+            data.put("action", "/sites/edit_site/"+id);
+            data.put("label", site.data.get("label"));
+            data.put("redirect_to", "/" + $.input.segment(0));
+            $.loadView("list_form", data);
+        } else {
+            site.updateSite($.input.post("label"));
+            $.response.sendRedirect($.input.post("redirect_to"));
+        }
+    }
+
+    public void add_task() throws Exception {
+        String id = $.input.segment(2);
+        PageData data = new PageData();
+        Site site = $.loadModel("Site"); // Load model  
+        Issue issue = $.loadModel("Issue"); // Load model  
+        if ($.input.post("label").isEmpty()) {
+            data.put("id", id);
+            data.put("label", "");
+            data.put("action", "/sites/add_task/"+id);
+            data.put("description", "");
+            data.put("redirect_to", "/" + $.input.segment(0));
+            $.loadView("list_form_add_task", data);
+        } else {
+            issue.insertIssue(id, $.input.post("label"), $.input.post("description"));
+            $.response.sendRedirect($.input.post("redirect_to"));
+        }
+    }
+}
