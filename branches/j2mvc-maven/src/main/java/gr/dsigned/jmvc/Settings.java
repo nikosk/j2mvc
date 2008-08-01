2930c524c78e6578eae28b19f4c2ef5a6462bdf0
cpeppas <cpeppas@f264b053-494f-0410-bddc-8b74c26815f4>
2008-08-01 13:46:55 +0000

    pom

--- branches/j2mvc-maven/src/main/java/gr/dsigned/jmvc/Settings.java
+++ branches/j2mvc-maven/src/main/java/gr/dsigned/jmvc/Settings.java
@@ -12,7 +12,6 @@ import java.util.Properties;
 public class Settings  {
    
     public static long fileTimeStamp = 0;
-    public static long c = 0;
     
     public enum AutoLoad{
     	SESSION(true);
@@ -27,39 +26,27 @@ public class Settings  {
     	}
     }  
   
-    public static Properties propertiesCached = null;
+    public static Properties properties = null;
     
     public static String get(String property) {
-        c++;
         String value = "";
-        System.out.println("Counter: " + c + " TimeStamp:" + fileTimeStamp);
+
         try {
             File file = new File(new URI(Settings.class.getResource("settings.properties").toString()));
-            System.out.println("Last modified before check:"+file.lastModified());
-            if (file.lastModified() > fileTimeStamp) {
+
+            if ( properties == null || file.lastModified() > fileTimeStamp) {
 
                 fileTimeStamp = file.lastModified();
-                Properties properties = new Properties();
+                properties = new Properties();
                 properties.load(new FileInputStream(file));
-
-                System.out.println("Reading from settings.properties file.....");
-                System.out.println(properties);
-                
                 value = properties.getProperty(property);
-                propertiesCached = properties;
             } else {
-                System.out.println("File is cached!");
-                value = propertiesCached.getProperty(property);
+                value = properties.getProperty(property);
             }
         } catch (Exception e) {
             System.out.println(e);
         }
 
         return value;
-    }
-    
-    public static Long getTime()throws Exception {
-        File file = new File(new URI(Settings.class.getResource("settings.properties").toString()));
-        return file.lastModified();
     }
 }
