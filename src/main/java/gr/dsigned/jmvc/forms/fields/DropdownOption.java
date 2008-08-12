e58b15e505aaa3ba1c7bd04dfada2a59d4d43952
vchrys <vchrys@f264b053-494f-0410-bddc-8b74c26815f4>
2008-08-12 07:43:30 +0000

    author name added

--- src/main/java/gr/dsigned/jmvc/forms/fields/DropdownOption.java
+++ src/main/java/gr/dsigned/jmvc/forms/fields/DropdownOption.java
@@ -1,15 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  DropdownOption.java
+ *
+ *  Copyright (C) 2008 Vas Chryssikou <nikosk@dsigned.gr>
+ *
+ *  This module is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version. See http://www.gnu.org/licenses/lgpl.html.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  */
 
 package gr.dsigned.jmvc.forms.fields;
 
 import gr.dsigned.jmvc.types.Tuple2;
-/**
- *
- * @author USER
+ /**
+ * @author Vas Chryssikou <vchrys@gmail.com>
  */
+
 public class DropdownOption {
     
     String template = "<option value='%1$s'>%2$s</option>%n";
