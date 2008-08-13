1e42d30cfb0a30131f2054c568c2ba4e9ead16ee
vchrys <vchrys@f264b053-494f-0410-bddc-8b74c26815f4>
2008-08-13 10:22:36 +0000

    for nik

--- branches/j2mvc-maven/src/main/java/gr/dsigned/jmvc/forms/NewForms.java
+++ branches/j2mvc-maven/src/main/java/gr/dsigned/jmvc/forms/NewForms.java
@@ -17,7 +17,17 @@ package gr.dsigned.jmvc.forms;
 import gr.dsigned.jmvc.forms.fields.Field;
 import gr.dsigned.jmvc.types.Bean;
 import gr.dsigned.jmvc.framework.Library;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
 
 /**
  *  
@@ -55,13 +65,57 @@ public class NewForms extends Library {
             this.fields.add(f); 
         }
     }
-    public boolean isValid(){
+    public boolean isValid(HttpServletRequest request){
         boolean valid = true;
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request) ;
+        if(isMultipart){
+            try {
+                ServletFileUpload upload = new ServletFileUpload();
+                FileItemIterator iter = upload.getItemIterator(request);
+                while (iter.hasNext()) {
+                    FileItemStream item = iter.next();
+                    String name = item.getFieldName();
+                    InputStream stream = item.openStream();
+                    if (item.isFormField()) {
+                        for(Field f : fields){
+                            if(f.getFieldName().equalsIgnoreCase(name)){
+                                f.setValue(Streams.asString(stream));
+                            }
+                        }
+                        //System.out.println("Form field " + name + " with value " + Streams.asString(stream) + " detected.");
+                    }
+                    else {
+                        for(Field f : fields){
+                            if(f.getFieldName().equalsIgnoreCase(name)){
+                                f.setValue(Streams.asString(stream));
+                            }
+                        }
+                        //System.out.println("File field " + name + " with file name " + item.getName() + " detected.");
+                        // Process the input stream
+                    }
+                }
+            }
+            catch (FileUploadException ex) {
+                Logger.getLogger(NewForms.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            catch (IOException ex) {
+                Logger.getLogger(NewForms.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         for(Field f : fields){
             if(!f.validates()){
                 valid = false;
             }
         }
+
         return valid;
+    }
+    
+    public Bean addFormValuesToBean(){
+        Bean bean = new Bean() ;
+        for(Field f : fields){
+            bean.put(f.getFieldName(), f.getValue()) ;
+        } 
+        return bean;
     }
 }
