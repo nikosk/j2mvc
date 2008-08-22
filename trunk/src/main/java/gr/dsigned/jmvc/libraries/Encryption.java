5d546c462e30c8fe5ba5bd6d1d1bf7ef70482f8b
nikos.nikos.nikos.nikos <nikos.nikos.nikos.nikos@f264b053-494f-0410-bddc-8b74c26815f4>
2008-08-22 15:47:04 +0000

    Added json library Removed unused classes

--- trunk/src/main/java/gr/dsigned/jmvc/libraries/Encryption.java
+++ trunk/src/main/java/gr/dsigned/jmvc/libraries/Encryption.java
@@ -1,8 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  DataForm.java
+ * 
+ *  Copyright (C) 2008 Christos Peppas <c.peppas@phiresoft.com>
+ * 
+ *  This module is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version. See http://www.gnu.org/licenses/lgpl.html.
+ * 
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ *  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  */
-
 package gr.dsigned.jmvc.libraries;
 
 import gr.dsigned.jmvc.framework.Library;
@@ -12,7 +21,7 @@ import java.security.NoSuchAlgorithmException;
 
 /**
  *
- * @author user
+ * @author Christos Peppas <c.peppas@phiresoft.com>
  */
 public class Encryption extends Library{
     
