2b2578490c399e6620dc80ab7de11c6b9d40fd9c
nikos.nikos.nikos.nikos <nikos.nikos.nikos.nikos@f264b053-494f-0410-bddc-8b74c26815f4>
2009-07-31 20:26:46 +0000

    Fixed Router to return alias methods with non aliased classes

--- trunk/src/test/java/gr/dsigned/jmvc/framework/RouterTest.java
+++ trunk/src/test/java/gr/dsigned/jmvc/framework/RouterTest.java
@@ -4,6 +4,9 @@
  */
 package gr.dsigned.jmvc.framework;
 
+import gr.dsigned.jmvc.controllers.CMSController;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.Assert;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -27,13 +30,35 @@ public class RouterTest {
     }
 
     @Test
-    public void testGetControllerName() {
+    public void testGetController() {
         Router r = new Router();
-        r.addControllerClass(SampleController.class);
-        Assert.assertEquals(r.getControllerName("samplecontroller"),"Samplecontroller");
-        Assert.assertEquals(r.getControllerName("controllertest"),"Samplecontroller");
-        Assert.assertEquals(r.getControllerName("controllertest/methodtest"),"Samplecontroller");
-        Assert.assertEquals(r.getControllerName("controllertest/methodtest"),"Samplecontroller");
+        r.addControllerClass(CMSController.class);
+
+        Assert.assertEquals(r.getControllerName("cmscontroller"), "Cmscontroller");
+        Assert.assertEquals(r.getControllerName("cmscontroller"), "Cmscontroller");
+        Assert.assertEquals(r.getControllerName("controllertest/methodtest"), "Cmscontroller");
+        Assert.assertEquals(r.getControllerName("controllertest/methodtest"), "Cmscontroller");
+
+        Assert.assertEquals(r.getControllerClassByReqURI("controllertest/methodtest"), CMSController.class);
+        Assert.assertEquals(r.getControllerClassByReqURI("cmscontroller/methodtest"), CMSController.class);
+        Assert.assertEquals(r.getControllerClassByReqURI("cmscontroller/index"), CMSController.class);
+        Assert.assertEquals(r.getControllerClassByReqURI("controllertest/index"), CMSController.class);
+
+        Assert.assertEquals(r.getMethodName("controllertest/index"), "index");
+        Assert.assertEquals(r.getMethodName("cmscontroller/index"), "index");
+        Assert.assertEquals(r.getMethodName("controllertest/methodtest"), "index");
+        Assert.assertEquals(r.getMethodName("cmscontroller/methodtest"), "index");
+        
+        try {
+            Assert.assertEquals(r.getMethodClassByReqURI("controllertest/index"), CMSController.class.getMethod("index", new Class[0]));
+            Assert.assertEquals(r.getMethodClassByReqURI("cmscontroller/index"), CMSController.class.getMethod("index", new Class[0]));
+            Assert.assertEquals(r.getMethodClassByReqURI("controllertest/methodtest"), CMSController.class.getMethod("index", new Class[0]));
+            Assert.assertEquals(r.getMethodClassByReqURI("cmscontroller/methodtest"), CMSController.class.getMethod("index", new Class[0]));
+        } catch (Exception ex) {
+            //Do nothing
+            Assert.fail(ex.getMessage());
+        }
+
     }
 
     @Test
